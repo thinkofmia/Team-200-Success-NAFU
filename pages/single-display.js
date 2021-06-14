81278e87376d898f711c7f7681a627cc45e57c67
thinkofmia <thinkofmia@gmail.com>
2021-06-14 17:54:14 +0800

    Shifted fake data in global variables

--- pages/single-display.js
+++ pages/single-display.js
@@ -4,30 +4,22 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import * as WebBrowser from 'expo-web-browser';
 
-const TEST_EVENT = {
-    id : "1",
-    title : "Polar Bear found drinking by West Coast Park",
-    image : 'https://www.themebeta.com/media/cache/400x225/files/windows/images/201907/26/3872e0062b2df0aecdd5b6568f5e16d9.jpeg',
-    text : "Wanna get drunk and groove with polar bears on a Monday Blues in a Post Covid Era? Join us and DJ Magnus as we set up a chilly bar for you in Magnus's Fusion Bar today. Register now!",
-    link : 'https://steamcommunity.com/id/Magfuse'
-}
-
 export default function SingleDisplay({navigation}) {
   return (
     <View style={styles.container}>
-        <Text style={styles.eventsHeader}>{TEST_EVENT.title}</Text>
+        <Text style={styles.eventsHeader}>{global.fakeArticle.title}</Text>
         <View style={styles.imgBackground}>
           <Image
             style={styles.eventsImage}
             source={{
               uri:
-                TEST_EVENT.image
+              global.fakeArticle.image
             }}
           />
         </View>
         <View style={styles.flexContainer}>
-            <Text style={styles.eventsText}>{TEST_EVENT.text}</Text>
-            <Button onPress={() => Linking.openURL(TEST_EVENT.link)} title="Link to Party"/>
+            <Text style={styles.eventsText}>{global.fakeArticle.text}</Text>
+            <Button onPress={() => Linking.openURL(global.fakeArticle.link)} title="Link to Party"/>
         </View>
     </View>
     );
