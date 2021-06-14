d3ca8468d85740b550df82f19231013b685c81ac
Nicholas Neo <nicholasneo78@gmail.com>
2021-06-14 12:24:30 +0800

    nic test

--- App.js
+++ App.js
@@ -1,36 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-import SingleDisplayScreen from './pages/single-display';
-import MainPage from "./pages/mainPage"
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SingleDisplayScreen from "./pages/single-display";
+import MainPage from "./pages/mainPage";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>,
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={MainPage}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    (
+      <View style={styles.container}>
+        <Text> Open up App.js to start working on your app! Mao is here!</Text>{" "}
+        <StatusBar style="auto" />
+      </View>
+    ),
+    (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={MainPage} />{" "}
+        </Stack.Navigator>{" "}
+      </NavigationContainer>
+    )
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
-
