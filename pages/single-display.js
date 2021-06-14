b2f51a54043f2d3dee78ab4ae835d809f5e3f134
thinkofmia <thinkofmia@gmail.com>
2021-06-14 13:54:58 +0800

    Added dummy article

--- pages/single-display.js
+++ pages/single-display.js
@@ -2,36 +2,51 @@ import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Text, Image } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import * as WebBrowser from 'expo-web-browser';
 
 export default function SingleDisplay({navigation}) {
   return (
     <View style={styles.container}>
+        <Text style={styles.eventsHeader}>Polar Bear found drinking by West Coast Park</Text>
         <View style={styles.imgBackground}>
           <Image
             style={styles.eventsImage}
             source={{
               uri:
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWy1rhx6EXXOp-o6Jh89UM-FYySA8Ic34LFQ&usqp=CAU',
+                'https://www.themebeta.com/media/cache/400x225/files/windows/images/201907/26/3872e0062b2df0aecdd5b6568f5e16d9.jpeg'
             }}
           />
         </View>
-        <Text style={styles.eventsText}>Link to events</Text>
+        <Text style={styles.eventsText}>Wanna get drunk and groove with polar bears on a Monday Blues in a Post Covid Era? Join us and DJ Magnus as we set up a chilly bar for you in 
+            <Text style={styles.linkText}> Magnus's Fusion Bar </Text> 
+        today. Register now!</Text>
     </View>
     );
   }
 
 const styles=StyleSheet.create({
   imgBackground:{
-    width: "70%",
+    width: "90%",
+    height: "50%",
     backgroundColor: "blue",
   },
+  eventsHeader: {
+    fontSize: 40,
+    fontWeight: "bold",
+    top: 0,
+  },
+  linkText: {
+      color: "blue",
+      fontWeight: "bold",
+  },
   eventsText: {
-    fontWeight: "bold"
+    padding: 10,
+    fontSize: 15,
+    marginTop: 20,
   },
   eventsImage: {
-    height: 120,
-    width: 120,
-    borderRadius: 60,
+    height: "100%",
+    width: "100%",
     alignSelf: 'center',
     marginTop: 10,
     borderColor: 'white',
