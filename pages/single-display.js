686e7228218488ae750c120e0cac22228fc3d985
thinkofmia <thinkofmia@gmail.com>
2021-06-14 15:22:09 +0800

    Added button to single display

--- pages/single-display.js
+++ pages/single-display.js
@@ -1,8 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Text, Image, Linking } from 'react-native';
+import { View, StyleSheet, Text, Image, Linking, Button } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import * as WebBrowser from 'expo-web-browser';
+
+const TEST_EVENT = {
+    id : "1",
+    title : "Polar Bear found drinking by West Coast Park",
+    image : 'https://www.themebeta.com/media/cache/400x225/files/windows/images/201907/26/3872e0062b2df0aecdd5b6568f5e16d9.jpeg',
+    text : "Wanna get drunk and groove with polar bears on a Monday Blues in a Post Covid Era? Join us and DJ Magnus as we set up a chilly bar for you in Magnus's Fusion Bar today. Register now!",
+    link : 'https://steamcommunity.com/id/Magfuse'
+}
 
 export default function SingleDisplay({navigation}) {
   return (
@@ -19,6 +27,7 @@ export default function SingleDisplay({navigation}) {
         </View>
         <View style={styles.flexContainer}>
             <Text style={styles.eventsText}>Wanna get drunk and groove with polar bears on a Monday Blues in a Post Covid Era? Join us and DJ Magnus as we set up a chilly bar for you in <Text style={styles.linkText} onPress={() => Linking.openURL('https://steamcommunity.com/id/Magfuse')}>Magnus's Fusion Bar </Text>today. Register now!</Text>
+            <Button onPress={() => Linking.openURL('https://steamcommunity.com/id/Magfuse')} title="Link to Party"/>
         </View>
     </View>
     );
