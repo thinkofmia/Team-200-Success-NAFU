e3f9a3ecf68d24ea4ece5b39c80a93e0275332f8
thinkofmia <thinkofmia@gmail.com>
2021-06-14 12:12:25 +0800

    Create single page display and install react navigation

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SingleDisplayScreen from './pages/single-display';
 
 export default function App() {
   return (
---
+++ pages/single-display.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, Text, Image } from 'react-native';
+
+function SingleDisplayScreen({ navigation }) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Image
+          style={styles.profilePic}
+          source={{
+            uri:
+              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWy1rhx6EXXOp-o6Jh89UM-FYySA8Ic34LFQ&usqp=CAU',
+          }}
+        />
+        <Text>Link to events</Text>
+      </View>
+    );
+  }
+
+const styles=StyleSheet.create({
+  text: {
+    fontWeight: "bold"
+  },
+  profilePic: {
+    height: 120,
+    width: 120,
+    borderRadius: 60,
+    alignSelf: 'center',
+    marginTop: 10,
+    borderColor: 'white',
+    borderWidth: 2,
+  },
+})
