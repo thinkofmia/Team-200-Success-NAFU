ad19202f912e3f2155587d52fbe1b97defb89426
siewyeng <siewyeng.c@gmail.com>
2021-06-14 12:07:38 +0800

    top navigation to main

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MainPage from "./pages/mainPage"
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </View>,
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MainPage}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -19,3 +31,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
