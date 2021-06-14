59b4ad8b914a9bb31870026f57eff1996434ac24
thinkofmia <thinkofmia@gmail.com>
2021-06-14 12:49:09 +0800

    Added display css

--- pages/single-display.js
+++ pages/single-display.js
@@ -5,30 +5,36 @@ import { NavigationContainer } from '@react-navigation/native';
 
 export default function SingleDisplay() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Image
-          style={styles.profilePic}
-          source={{
-            uri:
-              'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWy1rhx6EXXOp-o6Jh89UM-FYySA8Ic34LFQ&usqp=CAU',
-          }}
-        />
-        <Text>Link to events</Text>
-      </View>
-    );
-  }
-
-const styles=StyleSheet.create({
-  text: {
-    fontWeight: "bold"
-  },
-  profilePic: {
-    height: 120,
-    width: 120,
-    borderRadius: 60,
-    alignSelf: 'center',
-    marginTop: 10,
-    borderColor: 'white',
-    borderWidth: 2,
-  },
-})
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <View style={styles.imgBackground}>
+            <Image
+              style={styles.eventsImage}
+              source={{
+                uri:
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWy1rhx6EXXOp-o6Jh89UM-FYySA8Ic34LFQ&usqp=CAU',
+              }}
+            />
+          </View>
+          <Text style={styles.eventsText}>Link to events</Text>
+        </View>
+      );
+    }
+  
+  const styles=StyleSheet.create({
+    imgBackground:{
+      width: "70vw",
+      backgroundColor: "blue",
+    },
+    eventsText: {
+      fontWeight: "bold"
+    },
+    eventsImage: {
+      height: 120,
+      width: 120,
+      borderRadius: 60,
+      alignSelf: 'center',
+      marginTop: 10,
+      borderColor: 'white',
+      borderWidth: 2,
+    },
+  })
