f301cbb852030e0f3f0f8c16927c67e0666fa4d6
thinkofmia <thinkofmia@gmail.com>
2021-06-15 21:37:37 +0800

    Added filter based on preferences

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,4 +1,16 @@
-function filterData(listArray, filterOptions){
-     
-    return listArray;
+export function filterData(listArray, filterOptions, categories){
+    var filteredArr = [];
+    var tempArr = [];
+
+    if (categories.length==0) tempArr=listArray;
+    else {
+        for (var i=0;i<listArray.length;i++){
+            if (categories.includes(listArray[i].category)) tempArr.push(listArray[i]);
+        }
+    }
+
+    filteredArr = tempArr;
+    console.log("Filtered Array: ")
+    console.log(filteredArr);
+    return filteredArr;
 }
